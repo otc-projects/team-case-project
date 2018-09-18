9d60065672c58366807e5f10a377d80dee3db511
Adam Oates <imadorkface@gmail.com>
2018-09-18 17:45:22 -0500

    Edited adam.js
    I edited the adam.js file to make the transition between each image the user clicks smoother.

--- js/adam.js
+++ js/adam.js
@@ -1,11 +1,37 @@
+var img = {
+    Fennec_Fox_Small: "img/Fennec_Fox.jpg",
+    fennec_running_sand_small: "img/fennec_running_sand.jpg",
+    four_fennec_small: "img/four_fennec.jpg"
+};
+
 window.onload = function() {
     
-    document.getElementById("newsLttrBttn").addEventListener("click", function() {
-        newsLetterSuccess(document.getElementById("newsLttrField").value);
-    });
+    var thmbC = document.querySelector("div#thumbnailContainer");
+    
+    document.getElementById("newsLttrBttn").addEventListener("click", newsLetterSuccess, false);
+    
+    // Adds an event listener for each img element under the #thumbNailContainer div.
+    var thumb = document.querySelectorAll("div#thumbnailContainer img");
+    for (var i = 0; i < thumb.length; i++)
+        thumb[i].addEventListener("click", thumbClick, false);
     
 };
 
-function newsLetterSuccess(e) {
-    alert("You have successfully signed up for newsletters with " + e + ".\nNOTE: This was just a test you haven't signed up for anything.");
+function newsLetterSuccess() {
+    var e = document.getElementById("newsLttrField").value;
+    document.querySelector("p:last-of-type").innerHTML = "You have successfully signed up for newsletters with " + e + ".";
+}
+
+function thumbClick() {
+    // Get's the name of the image without the directory or extension.
+    var name = this.getAttribute("src").split("/")[1].split(".")[0];
+    var newImg = document.createElement("img");
+    newImg.src = img[name];
+    newImg.width = 640;
+    
+    // Removes the current enlarged image. and replaces it with the selected one.
+    newImg.onload = function() {
+        document.querySelector("div#imgContainer").removeChild(document.querySelector("div#imgContainer img"));
+        document.querySelector("div#imgContainer").appendChild(newImg);
+    };
 }
